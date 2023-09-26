9043d8e5342e7863b15b045ee1093fbdec0d5f9b
L-Pircy <taveesha.agarwal@gmail.com>
2023-09-26 11:32:58 +0530

    some input

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,13 +1,15 @@
 "use client";
-import React from "react";
+import { useState, Fragment } from "react";
 import { SearchManufacturerProps } from "@/types";
 import { Combobox, Transition } from "@headlessui/react";
+import { manufacturers } from "@/constants";
 import Image from "next/image";
 
 const SearchManufacturer = ({
   manufacturer,
   setManufacturer,
 }: SearchManufacturerProps) => {
+  const [query, setQuery] = useState("");
   return (
     <div className="search-manufacturer">
       <Combobox>
@@ -21,6 +23,21 @@ const SearchManufacturer = ({
               alt="Car-Logo"
             />
           </Combobox.Button>
+          <Combobox.Input
+            className="search-manufacturer__input"
+            placeholder="Volkswagen"
+            displayValue={(manufacturer: string) => manufacturer}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+            afterLeave={() => setQuery("")}
+          >
+            <Combobox.Options></Combobox.Options>
+          </Transition>
         </div>
       </Combobox>
     </div>
